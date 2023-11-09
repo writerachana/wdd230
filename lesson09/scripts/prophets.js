61ec50e0dd5de97cf485b28934a19de7774ddc86
Rachana <rachanakumbhakar2019@gmail.com>
2023-11-08 21:58:42 -0700

    new

---
+++ lesson09/scripts/prophets.js
@@ -0,0 +1,43 @@
+// {
+//     "prophets": [
+//       {
+//         "name": "Joseph",
+//         "lastname": "Smith",
+//         "birthdate": "23 December 1805",
+//         "death": "27 June 1844",
+//         "length": 14,
+//         "order": 1,
+//         "birthplace": "Vermont",
+//         "numofchildren": 11,
+//         "imageurl": "https://assets.churchofjesuschrist.org/ec/f8/ecf80a88e31429d0aa0eeec6e2c74851a4bf4235/nauvoo_temple_joseph_art_lds.jpeg"
+//       }
+
+
+const url = " https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json";
+const cards = document.querySelector('#cards');
+
+function displayProphets(prophets){
+    prophets.forEach((prophet) => {
+        let section = document.createElement("section")
+        let sectionHTML =`<h2>${prophet.name} ${prophet. lastiane}<h2>
+        <p>Date of Birth: ${prophet.birthdate}</p>
+        <p>Place of Birth: ${prophet.birthplace}</p>
+        <img src="${prophet.imageurl}" alt="Picture of ${prophet.name} ${prophet.lastName}">`
+        section.innerHTML =sectionHTML;
+        cards.appendChild(section);
+        
+    });
+};
+
+async function getProphetData(){
+    const response = await fatch(url);
+    if(response,ok){
+        const data = await response.json();
+        displayProphets(data.prophets);
+
+    }
+    else{
+        console.error("No WORKIE!!!")
+
+    }
+}
